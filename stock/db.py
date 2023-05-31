d7f53f1f20222a1bb38ae83a1427c54d239f3082
Jeroen Nelen <jwnelen@gmail.com>
2023-05-31 15:20:58 +0200

    tests for stock are passing

--- stock/db.py
+++ stock/db.py
@@ -21,8 +21,12 @@ class StockDatabase:
         def trans_create_item(transaction):
             transaction.execute_update(
                 "INSERT INTO stock (item_id, price, amount) "
-                f"VALUES ('{item_id}', {price}, 0) "
-            )
+                f"VALUES ('{item_id}', {price}, 0) ")
+            # transaction.insert(
+            #     table="stock",
+            #     columns=("item_id", "price", "amount"),
+            #     values=[(item_id, price, 0)],
+            # )
 
         try:
             self.database.run_in_transaction(trans_create_item)
@@ -46,15 +50,16 @@ class StockDatabase:
 
     def add_stock(self, item_id, amount):
         def update_stock(transaction):
-            row_ct = transaction.execute_sql(
+            transaction.execute_update(
                 "UPDATE stock "
                 f"SET amount = amount + {amount} "
                 f"WHERE (item_id) = '{item_id}'"
             )
-
-            return {"amount_rows_affected": row_ct}
+            return {"done": True}
 
         try:
+            # This will do a read-modify-write with a transaction
+            # So it will do a begin and commit for you
             res = self.database.run_in_transaction(update_stock)
         except Exception as e:
             return {"error": str(e)}
@@ -62,14 +67,7 @@ class StockDatabase:
 
     def remove_stock(self, item_id, amount):
         def update_stock(transaction):
-            current_stock = transaction.execute_sql(
-                f"SELECT amount FROM stock WHERE item_id = '{item_id}'"
-            ).one_or_none()
-
-            if int(amount) > int(current_stock[0]):
-                return {"error": "not enough stock"}
-
-            transaction.execute_sql(
+            transaction.execute_update(
                 "UPDATE stock "
                 f"SET amount = amount - {amount} "
                 f"WHERE (item_id) = '{item_id}'"
