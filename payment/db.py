7aa213ed6d39a688ef8761ba86d6edb3c695b132
BigLob <raphael.fruehwirth@gmail.com>
2023-06-06 17:43:49 +0200

    first sqlalchemy change

--- payment/db.py
+++ payment/db.py
@@ -2,48 +2,47 @@ from uuid import uuid4
 
 from google.cloud import spanner
 
+import sqlalchemy, sys
+
 
 class UserDatabase:
     def __init__(self):
         instance_id = "spanner-db"
-        database_id = "database"
+        database_id = "database-staging"
+        project_id = "wdmproject23-v2"
 
-        # Instantiate a client.
-        spanner_client = spanner.Client()
+        # # Instantiate a client.
+        # spanner_client = spanner.Client()
+        # # Get a Cloud Spanner instance by ID.
+        # self.instance = spanner_client.instance(instance_id)
+        # self.database = self.instance.database(database_id)
 
-        # Get a Cloud Spanner instance by ID.
-        self.instance = spanner_client.instance(instance_id)
-        self.database = self.instance.database(database_id)
+        db_url = f"spanner+spanner:///projects/{project_id}/instances/{instance_id}/databases/{database_id}"
+        self.engine = sqlalchemy.create_engine(db_url)
+        self.connection = self.engine.connect()
+
+        print("@# TABLE NAMES", self.engine.dialect.get_table_names(self.connection), file=sys.stderr)
+
+        self.metadata = sqlalchemy.MetaData()
+        self.users = sqlalchemy.Table('users', self.metadata, autoload_with=self.engine)
 
     def create_user(self):
         u_id = str(uuid4())
 
-        def trans_create_user(transaction):
-            transaction.execute_update(
-                "INSERT INTO users (user_id, credit) "
-                f"VALUES ('{u_id}', 0) "
-            )
+        query = sqlalchemy.insert(self.users).values(user_id=u_id, credit=0)
+        result = self.connection.execute(query)
 
-        try:
-            self.database.run_in_transaction(trans_create_user)
-        except Exception as e:
-            return {"error": str(e)}
+        print("###CREATE USER", u_id, file=sys.stderr)
 
         return {"user_id": u_id}
 
     def find_user(self, user_id):
-        query = f"SELECT * FROM users WHERE user_id = '{user_id}'"
+        query = sqlalchemy.select(self.users).where(self.users.columns.user_id == user_id) 
+        result = self.connection.execute(query).fetchall()
 
-        with self.database.snapshot() as snapshot:
-            result = snapshot.execute_sql(query).one_or_none()
+        print("##FIND USER ", result, file=sys.stderr)
+        return {'user_id': result[0][0], 'credit': result[0][1]}
 
-            if result is None:
-                return {"error": "user_id does not exist"}
-
-            return {
-                "user_id": result[0],
-                "credit": float(result[1])
-            }
 
     def add_credit_to_user(self, user_id, amount):
         def update_user(transaction):
